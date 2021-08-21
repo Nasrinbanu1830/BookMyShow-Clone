2954e312806135214b9d7aeaa073ca304930bf54
Nasrinbanu1830 <nasrinsyed18@gmail.com>
2021-08-21 12:23:07 +0530

    project setup

---
+++ planning.js
@@ -0,0 +1,22 @@
+//   BOOKMYSHOW UI CLONE 
+    
+// we need a reusable caurosel
+   //  --> Hero Carousel
+   //  --> Common Carousel
+   //  --> Small Carousel(cast & crew)
+
+
+   //Pages
+    // --> Home Page
+    // --> Movie page
+    // --> Categories Page
+    // --> Auth Page
+    // --> Payment Gateway (Razorpay)
+
+
+       //  --- plan ---
+
+       // => layouts
+       // => Routes
+       // => pages
+       // => Components
